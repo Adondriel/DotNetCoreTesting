cc2bd259dcd8cfe2e222bf0b3834e74c58e74bd8
Adam Pine <adondrielcraft@gmail.com>
2017-04-20 16:09:33 -0400

    2

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AdamPineDotMe
+namespace WebApplicationBasic
 {
     public class Startup
     {
